01cbc7e887bef944730bcbbcb38ed5ba492af97e
Poonyapat <balesacottish4@gmail.com>
2023-09-27 13:21:42 +0700

    testmerge

--- src/screens/home/sugar/page2.js
+++ src/screens/home/sugar/page2.js
@@ -29,7 +29,7 @@ import Header from '../../../components/header'
  * @param {(value: string) => void} props.onChanged
  */
 
-const Page2 = ({navigation,props}) => {
+const Page2 = ({ navigation, props }) => {
 
   const testrec = testrecom['0']
 
@@ -44,29 +44,36 @@ const Page2 = ({navigation,props}) => {
 
         <ScrollView>
           <View style={{ flexDirection: 'row', justifyContent: 'space-between', }}>
+
             <View style={{ width: '50%', marginRight: 15, justifyContent: 'center', alignItems: 'center', }}>
+
               <View style={{ justifyContent: 'center', alignItems: 'center', width: 35, height: 35, borderColor: colors.blue_transparent_5, borderRadius: 20, borderWidth: 1 }}>
                 <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.subTitle, color: colors.blue_transparent_5, borderRadius: 20, }}>2</Text>
               </View>
-              <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.body, color: colors.bule, marginTop: 10 }}>{testrec.description}</Text>
+              <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.body, color: colors.bule, marginTop: 10 }}>เปิดสวิชท์เครื่องด้านข้าง แล้วหน้าจอแสดงผลบนเครื่องเปิดขึ้น</Text>
             </View>
             <Image source={require('../../../assets/images/st.png')} style={styles.Imageheader} />
           </View>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10, }}>
-            <View style={{ width: '50%', marginRight: 15, justifyContent: 'center', alignItems: 'center', }}>
-              <View style={{ justifyContent: 'center', alignItems: 'center', width: 35, height: 35, borderColor: colors.blue_transparent_5, borderRadius: 20, borderWidth: 1 }}>
-                <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.subTitle, color: colors.blue_transparent_5, borderRadius: 20, }}>2</Text>
-              </View>
-              <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.body, color: colors.bule, marginTop: 10 }}>{testrec.description}</Text>
-            </View>
-            <Image source={require('../../../assets/images/st.png')} style={styles.Imageheader} />
-          </View>
+
           <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10, }}>
             <View style={{ width: '50%', marginRight: 15, justifyContent: 'center', alignItems: 'center', }}>
               <View style={{ justifyContent: 'center', alignItems: 'center', width: 35, height: 35, borderColor: colors.blue_transparent_5, borderRadius: 20, borderWidth: 1 }}>
                 <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.subTitle, color: colors.blue_transparent_5, borderRadius: 20, }}>3</Text>
               </View>
-              <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.body, color: colors.bule, marginTop: 10 }}>{testrec.description}</Text>
+              <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.body, color: colors.bule, marginTop: 10 }}>เครื่องเมื่อเปิดเครื่องแล้ว ไฟแสดงสถานะ “สีแดง” (หมายถึง เครื่องกำลังเตรียมพร้อมสำหรับการเป่า) เมื่อเครื่องพร้อมทำงานก็จะมีีไฟแสดงสถานะขึ้น “สีเขียว” และมีตัวอักษรแสดงไว้ที่จอ คำว่า Blow และให้ทำการเป่าทันที
+                *** ไฟแสดงสถานะ จะใช้หลอดเดียวเป็น RGB แสดงได้สองสีในหลอดเดียว
+              </Text>
+            </View>
+            <Image source={require('../../../assets/images/st.png')} style={styles.Imageheader2} />
+          </View>
+
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10, }}>
+            <View style={{ width: '50%', marginRight: 15, justifyContent: 'center', alignItems: 'center', }}>
+              <View style={{ justifyContent: 'center', alignItems: 'center', width: 35, height: 35, borderColor: colors.blue_transparent_5, borderRadius: 20, borderWidth: 1 }}>
+                <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.subTitle, color: colors.blue_transparent_5, borderRadius: 20, }}>4</Text>
+              </View>
+              <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.body, color: colors.bule, marginTop: 10 }}>การเป่าลม ให้กดปุ่ม start อมที่หลอดเป่า แล้วเป่าด้วยลมแรงเสมอกันต้องไม่ขาดช่วง เครื่องจะนับถอยหลัง 15 วินาที จนมีเสียงเตือนให้หยุดเป่า
+              </Text>
             </View>
             <Image source={require('../../../assets/images/st.png')} style={styles.Imageheader} />
           </View>
@@ -79,7 +86,7 @@ const Page2 = ({navigation,props}) => {
             containerStyle={{
               paddingHorizontal: 16,
               marginTop: 30,
-              marginBottom:40,
+              marginBottom: 40,
               flexDirection: 'column',
               alignSelf: 'center',
               alignItems: 'center',
@@ -106,6 +113,12 @@ const styles = StyleSheet.create({
     width: 160,
     height: 240,
     borderRadius: 20
+  },
+  Imageheader2: {
+    width: 160,
+    height: 240,
+    borderRadius: 20,
+    marginTop: 40
   },
 
 })
--- src/screens/home/sugar/page3.js
+++ src/screens/home/sugar/page3.js
@@ -29,7 +29,7 @@ import Header from '../../../components/header'
  */
 
 
-const Page3 = ({navigation,props}) => {
+const Page3 = ({ navigation, props }) => {
 
   const testrec = testrecom['0']
 
@@ -40,25 +40,28 @@ const Page3 = ({navigation,props}) => {
         ...globalStyles2.welcome_padding,
       }}>
         <ScrollView>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', }}>
-            <View style={{ width: '50%', marginRight: 15, justifyContent: 'center', alignItems: 'center', }}>
-              <View style={{ justifyContent: 'center', alignItems: 'center', width: 35, height: 35, borderColor: colors.blue_transparent_5, borderRadius: 20, borderWidth: 1 }}>
-                <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.subTitle, color: colors.blue_transparent_5, borderRadius: 20, }}>4</Text>
-              </View>
-              <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.body, color: colors.bule, marginTop: 10 }}>{testrec.description}</Text>
-            </View>
-            <Image source={require('../../../assets/images/st.png')} style={styles.Imageheader} />
-          </View>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop:20}}>
             <View style={{ width: '50%', marginRight: 15, justifyContent: 'center', alignItems: 'center', }}>
               <View style={{ justifyContent: 'center', alignItems: 'center', width: 35, height: 35, borderColor: colors.blue_transparent_5, borderRadius: 20, borderWidth: 1 }}>
                 <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.subTitle, color: colors.blue_transparent_5, borderRadius: 20, }}>5</Text>
               </View>
-              <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.body, color: colors.bule, marginTop: 10 }}>{testrec.description}</Text>
+              <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.body, color: colors.bule, marginTop: 10 }}>เมื่อหยุดเป่าแล้ว เครื่องจะทำการประเมินผล และอ่านค่าให้ที่ จอภาพแสดงผล ว่ามีระดับน้ำตาลในเลือดเท่าไหร่
+                *** ค่าที่แสดงผลบนจอ จะแสดงผลเป็น BHB, ACE, ระดับน้ำตาล
+              </Text>
             </View>
             <Image source={require('../../../assets/images/st.png')} style={styles.Imageheader} />
           </View>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 20 }}>
+            <View style={{ width: '50%', marginRight: 15, justifyContent: 'center', alignItems: 'center', }}>
+              <View style={{ justifyContent: 'center', alignItems: 'center', width: 35, height: 35, borderColor: colors.blue_transparent_5, borderRadius: 20, borderWidth: 1 }}>
+                <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.subTitle, color: colors.blue_transparent_5, borderRadius: 20, }}>6</Text>
+              </View>
+              <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.body, color: colors.bule, marginTop: 10 }}>เมื่อเครื่องแสดงผลที่หน้าจอแล้ว ทำการปิดสวิชท์เครื่องด้านข้าง </Text>
+            </View>
+            <Image source={require('../../../assets/images/st.png')} style={styles.Imageheader} />
+          </View>
+
+          {/* <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
             <View style={{ width: '50%', marginRight: 15, justifyContent: 'center', alignItems: 'center', }}>
               <View style={{ justifyContent: 'center', alignItems: 'center', width: 35, height: 35, borderColor: colors.blue_transparent_5, borderRadius: 20, borderWidth: 1 }}>
                 <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.subTitle, color: colors.blue_transparent_5, borderRadius: 20, }}>6</Text>
@@ -66,7 +69,7 @@ const Page3 = ({navigation,props}) => {
               <Text style={{ fontFamily: fontFamily.regular, fontSize: fontSize.body, color: colors.bule, marginTop: 10 }}>{testrec.description}</Text>
             </View>
             <Image source={require('../../../assets/images/st.png')} style={styles.Imageheader} />
-          </View>
+          </View> */}
 
           <Botton
             text='ถัดไป'
